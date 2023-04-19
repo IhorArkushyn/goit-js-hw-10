3f10fcb2f7f3c2b5b22d835e9e30c860504d5e17
Ihor Arkushyn <snapscan68@gmail.com>
2023-04-19 19:06:58 +0300

    add hw_10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import { fetchCountries } from './fetchCountries';
 import './css/styles.css';
 
+// Глобальні змінні
 const DEBOUNCE_DELAY = 300;
+
+
