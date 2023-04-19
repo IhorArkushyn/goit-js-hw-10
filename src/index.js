c7aed151a4e00aef803376e8dcce231269a2fff8
Ihor Arkushyn <snapscan68@gmail.com>
2023-04-19 23:45:47 +0300

    upd hw_10

--- src/index.js
+++ src/index.js
@@ -6,4 +6,86 @@ import './css/styles.css';
 // Глобальні змінні
 const DEBOUNCE_DELAY = 300;
 
+const refs = {
+  inputEl: document.querySelector('#search-box'),
+  countriesList: document.querySelector('.country-list'),
+  countriesInfo: document.querySelector('.country-info'),
+};
 
+// слухач події
+refs.inputEl.addEventListener('input', debounce(fetchByInput, DEBOUNCE_DELAY));
+
+// Отримання результату з сервера
+function fetchByInput() {
+  const country = refs.inputEl.value.trim();
+  if (!country) {
+    clearMarkup();
+    return;
+  }
+  return fetchCountries(country).then(renderCountries).catch(showError);
+}
+
+// Очищення розмітки
+function clearMarkup() {
+  refs.countriesList.innerHTML = '';
+  refs.countriesInfo.innerHTML = '';
+}
+
+// Відображення помилки
+function showError() {
+  clearMarkup();
+  return Notiflix.Notify.failure('Oops, there is no country with that name');
+}
+
+// Варіанти рендерінгу згідно введених даних
+function renderCountries(countriesName) {
+  clearMarkup();
+  if (countriesName.length > 10) {
+    return Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  }
+  if (countriesName.length >= 2 && countriesName.length <= 10) {
+    renderCountryInfo(countriesName);
+  } else {
+    renderCountryInfo(countriesName);
+    renderCountryData(countriesName);
+  }
+}
+
+// Рендерінг розмітки варіанта декілька країни
+function renderCountryInfo(countriesName) {
+  const markupCountry = countriesName
+    .map(({ name, flags }) => {
+      return `<li class = "country-list__item">
+                <img src="${flags.svg}" alt="${name.common}" width="60" height="45">
+                <span class = "country-list__preview">${name.official}</span>
+              </li>`;
+    })      
+    .join('');
+  refs.countriesList.innerHTML = markupCountry;
+}
+
+// Рендерінг розмітки варіанту однієї країни з додатковою інформацією
+function renderCountryData(countriesName) {
+  clearMarkup();
+  const markupInfo = countriesName
+    .map(({ name, flags, capital, population, languages }) => {
+      return `
+            <div class="country-list__item">
+              <img src="${flags.svg}" alt="${name.common}" width="60" height="45">
+              <span class = "country-list__name">${name.official}</span>
+            </div>
+              <p class = "country-info__data"><b>Capital:</b> ${capital}</p>
+              <p class = "country-info__data"><b>Population:</b> ${population}</p>
+              <p class = "country-info__data"><b>Languages:</b> ${Object.values(
+                languages
+              )}</p>`;
+    })
+    .join('');
+  refs.countriesInfo.innerHTML = markupInfo;
+}
+
+const galleryContainer = document.querySelector('ul.gallery');
+
+galleryContainer.insertAdjacentHTML('beforeend', addGalleryMarcup);
